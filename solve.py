96dd654cf4227d3fb6e06f0d547e62dcc52a84c5
Andrew Tolopko <atolopko@chanzuckerberg.com>
2020-12-07 09:20:54 -0500

    Assert correct solution & fixes
    - Assert that the solution is in fact found and correct!
    - Fix query_response() impl for edge cases
    - Minor cleanup & refactoring

--- solve.py
+++ solve.py
@@ -4,83 +4,90 @@ import numpy as np
 import pdb
 
 # code repr is 4-char string, with each color as digit 0-5
-colors = 6
-code_len = 4
-max_codes = colors ** code_len
-all_codes = [np.base_repr(code, 6).rjust(code_len, '0') for code in range(0, max_codes)]
+COLORS = 6
+CODE_LEN = 4
+MAX_CODES = COLORS ** CODE_LEN
+ALL_CODES = [np.base_repr(code, 6).rjust(CODE_LEN, '0') for code in range(0, MAX_CODES)]
 
-initial_guess = all_codes[randrange(0, max_codes)]
 
-# print(all_codes)
-
-def next_code_query(admissable_codes):
+def next_code_query(admissible_codes):
     """ 
     As the code breaker, generate the next code query (guess). Do this
-    by simply guessing one of the remaining admissable_codes!
+    by simply guessing one of the remaining admissible_codes!
     """
-    return admissable_codes[randrange(len(admissable_codes))]
+    return admissible_codes[randrange(len(admissible_codes))]
 
 
 def query_response(query, code) -> (int, int):
     correct = 0
-    misplaced = 0
+    included = 0
     code = list(code)
-    for i in range(0, code_len):
-        c = query[i]
-        if c == code[i]:
+    query = list(query)
+
+    for c, q in zip(query, code):
+        if c == q:
             correct += 1
-            code[i] = None
-    for i in range(0, code_len):
-        c = query[i]
-        for j in range(0, code_len):
-            if c == code[j]:
-                misplaced += 1
-                code[j] = None
-                break
-    return correct, misplaced
+
+    code.sort()
+    query.sort()
+    for q in query:
+        if q in code:
+            code.remove(q)
+            included += 1
+
+    return correct, included - correct
 
 
-def code_admissible(query, code, resp_correct, resp_misplaced) -> bool:
-    correct, misplaced = query_response(query=code, code=query)
+def is_code_admissible(candidate_code, query, resp_correct, resp_misplaced) -> bool:
+    # For determining admissibility of a candidate code, the player's
+    # query becomes the code, and candidate code becomes the query
+    correct, misplaced = query_response(query=candidate_code, code=query)
     return correct == resp_correct and misplaced == resp_misplaced
 
 
-def prune_solutions(query, resp_correct, resp_misplaced, admissable_codes):
-    new_admissable_codes = []
-    for candidate_code in admissable_codes:
-        if code_admissible(query, candidate_code, resp_correct=resp_correct, resp_misplaced=resp_misplaced):
-            new_admissable_codes.append(candidate_code)
+def prune(query, resp_correct, resp_misplaced, admissible_codes):
+    new_admissible_codes = []
+    for candidate_code in admissible_codes:
+        if is_code_admissible(candidate_code, query, resp_correct=resp_correct, resp_misplaced=resp_misplaced):
+            new_admissible_codes.append(candidate_code)
             # print(f'admit {code}')
-    return new_admissable_codes
+    return new_admissible_codes
 
 
 def play_round(code, admissible_codes):
     query = next_code_query(admissible_codes)
     num_correct, num_misplaced = query_response(query=query, code=code)
-    new_admissible_codes = prune_solutions(query, num_correct, num_misplaced, admissible_codes)
-    # print(f'code={code}, query={query}, correct={num_correct}, misplaced={num_misplaced}, admissible_codes={len(new_admissible_codes)}')
+    new_admissible_codes = prune(query, num_correct, num_misplaced, admissible_codes)
+    print(f'code={code}, query={query}, correct={num_correct}, misplaced={num_misplaced}, admissible_codes={len(new_admissible_codes)}')
+
+    # Ensure progress is made
     assert len(new_admissible_codes) < len(admissible_codes)
-    # print(new_admissible_codes)
+
+    print(new_admissible_codes)
     return new_admissible_codes
 
         
 if __name__ == "__main__":
-    # code = sys.argv[1]
-    # assert code in all_codes
-
-    admissible_codes = all_codes
+    admissible_codes = ALL_CODES
     rounds_taken = []
 
-    for code in all_codes:
-        # print(f'Trying to break code {code}')
-        round = 0
-        admissible_codes = all_codes
+    for code in ALL_CODES:
+        print('=' * 80)
+        print(f'Trying to break code {code}')
+        round = 1
+        admissible_codes = ALL_CODES
         while len(admissible_codes) > 1:
-            # print(f'ROUND {round}')
+            print(f'ROUND {round}')
             admissible_codes = play_round(code, admissible_codes)
-            if len(admissible_codes) == 1:
-                print(f'{code} BROKEN in {round} rounds!')
             round += 1
+
+        assert len(admissible_codes) == 1
+
+        print(f'CODE: {code}')
+        print(f'QUERY: {admissible_codes[0]}')
+        assert admissible_codes[0] == code
+        print(f'{code} BROKEN in {round} rounds!')
+            
         rounds_taken.append(round)
 
     print(np.histogram(rounds_taken, bins=10, range=(0, 10)))
--- test_solve.py
+++ test_solve.py
@@ -1,4 +1,4 @@
-from solve import query_response, code_admissible, prune_solutions
+from solve import query_response, is_code_admissible, prune
 
 def test_query_response():
     assert query_response('1111', '1111') == (4, 0)
@@ -9,45 +9,46 @@ def test_query_response():
     assert query_response('0000', '1111') == (0, 0)
     assert query_response('1234', '0123') == (0, 3)
     assert query_response('1111', '1234') == (1, 0)
+    assert query_response('1450', '0000') == (1, 0)
     
 
-def test_code_admissible():
-    assert code_admissible('1234', '0100', 0, 1)
-    assert code_admissible('1234', '0010', 0, 1)
-    assert code_admissible('1234', '0001', 0, 1)
-    assert code_admissible('1234', '2000', 0, 1)
-    assert code_admissible('1234', '0020', 0, 1)
-    assert code_admissible('1234', '0002', 0, 1)
-    assert code_admissible('1234', '0100', 0, 1)
-    assert not code_admissible('1234', '2100', 0, 1)
+def test_is_code_admissible():
+    assert is_code_admissible('0100', '1234', 0, 1)
+    assert is_code_admissible('0010', '1234', 0, 1)
+    assert is_code_admissible('0001', '1234', 0, 1)
+    assert is_code_admissible('2000', '1234', 0, 1)
+    assert is_code_admissible('0020', '1234', 0, 1)
+    assert is_code_admissible('0002', '1234', 0, 1)
+    assert is_code_admissible('0100', '1234', 0, 1)
+    assert not is_code_admissible('2100', '1234', 0, 1)
 
-    assert code_admissible('1234', '2100', 0, 2)
-    assert not code_admissible('1234', '2130', 0, 2)
-    assert not code_admissible('1234', '2104', 0, 2)
+    assert is_code_admissible('2100', '1234', 0, 2)
+    assert not is_code_admissible('2130', '1234', 0, 2)
+    assert not is_code_admissible('2104', '1234', 0, 2)
 
-    assert code_admissible('1234', '1111', 1, 0)
-    assert not code_admissible('1234', '1211', 1, 0)
+    assert is_code_admissible('1111', '1234', 1, 0)
+    assert not is_code_admissible('1211', '1234', 1, 0)
 
-    assert code_admissible('1234', '1121', 1, 1)
-    assert code_admissible('1234', '1112', 1, 1)
-    assert not code_admissible('1234', '1114', 1, 1)
-    assert not code_admissible('1234', '1142', 1, 1)
+    assert is_code_admissible('1121', '1234', 1, 1)
+    assert is_code_admissible('1112', '1234', 1, 1)
+    assert not is_code_admissible('1114', '1234', 1, 1)
+    assert not is_code_admissible('1142', '1234', 1, 1)
 
-    assert code_admissible('1234', '1241', 2, 1)
-    assert not code_admissible('1234', '1243', 2, 1)
-    assert not code_admissible('1121', '1112', 2, 1)
-    assert code_admissible('1121', '1113', 2, 1)
+    assert is_code_admissible('1241', '1234', 2, 1)
+    assert not is_code_admissible('1243', '1234', 2, 1)
+    assert not is_code_admissible('1112', '1121', 2, 1)
+    assert is_code_admissible('1113', '1121', 2, 1)
 
-    assert not code_admissible('1234', '1241', 1, 2)
-    assert code_admissible('1234', '1321', 1, 2)
+    assert not is_code_admissible('1241', '1234', 1, 2)
+    assert is_code_admissible('1321', '1234', 1, 2)
 
-    assert not code_admissible('1234', '1234', 1, 3)
-    assert code_admissible('1234', '1423', 1, 3)
-    assert not code_admissible('1234', '1243', 1, 3)
+    assert not is_code_admissible('1234', '1234', 1, 3)
+    assert is_code_admissible('1423', '1234', 1, 3)
+    assert not is_code_admissible('1243', '1234', 1, 3)
 
 
-def test_prune_solutions():
-    assert prune_solutions('1234', 0, 0, ['1000', '0100', '2000', '0000', '0055', '5555']) == ['0000', '0055', '5555']
-    assert prune_solutions('1234', 1, 1, ['1020', '1300']) == ['1020', '1300']
-    assert prune_solutions('1234', 1, 1, ['1200', '1004']) == []
-    assert prune_solutions('1234', 2, 2, ['1324', '1243', '1432', '4231', '4321', '1111', '1234']) == ['1324', '1243', '1432', '4231']
+def test_prune():
+    assert prune('1234', 0, 0, ['1000', '0100', '2000', '0000', '0055', '5555']) == ['0000', '0055', '5555']
+    assert prune('1234', 1, 1, ['1020', '1300']) == ['1020', '1300']
+    assert prune('1234', 1, 1, ['1200', '1004']) == []
+    assert prune('1234', 2, 2, ['1324', '1243', '1432', '4231', '4321', '1111', '1234']) == ['1324', '1243', '1432', '4231']
